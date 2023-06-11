7f9e71fbdad3ec744e914892d91a83f015c473df
gumelar <gumelar.wicaksono6@gmail.com>
2023-06-11 21:10:52 +0700

    server list barang

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors'); // Tambahkan baris ini
 
+server.use(cors()); // Gunakan middleware cors
 server.use(middlewares);
 server.use('', router);
 server.listen(process.env.PORT || 5000, () => {
