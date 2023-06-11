8362aa20d95dcf1f535b69d45d6ef9590b7287ff
gumelar <gumelar.wicaksono6@gmail.com>
2023-06-11 21:17:31 +0700

    server list barang

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require('cors'); // Tambahkan baris ini
+const cors = require('cors');
 
-server.use(cors()); // Gunakan middleware cors
+// Daftar origin yang diizinkan
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin tidak diizinkan oleh server'));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 server.use(middlewares);
 server.use('', router);
 server.listen(process.env.PORT || 5000, () => {
