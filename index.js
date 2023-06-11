ffe6d87098009b297a38e80ad0778bf64d7731a1
gumelar <gumelar.wicaksono6@gmail.com>
2023-06-11 21:55:11 +0700

    server list barang

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const corsOptions = {
 server.use(cors(corsOptions));
 server.use(middlewares);
 server.use('', router);
+
+// Tambahkan header Access-Control-Allow-Origin secara manual
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigins);
+  next();
+});
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running');
 });
