039e916de1719e5f50de94554ba0bf686f1d35af
gumelar <gumelar.wicaksono6@gmail.com>
2023-06-12 14:02:52 +0700

    create server list barang

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
 // Daftar origin yang diizinkan
+const corsOptions = {
+  origin: '*', // Atur origin sesuai dengan kebutuhan Anda
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
 
 server.use(cors(corsOptions));
 server.use((req, res, next) => {
