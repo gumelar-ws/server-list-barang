cf429f1afef398b7c4f1181a22211c5a30ee3ced
gumelar <gumelar.wicaksono6@gmail.com>
2023-06-12 13:41:55 +0700

    create server list barang

--- index.js
+++ index.js
@@ -2,38 +2,17 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require('cors');
 
 // Daftar origin yang diizinkan
-const allowedOrigins = ['http://localhost:3000', 'https://list-barang.netlify.app/'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Origin tidak diizinkan oleh server'));
-    }
-  },
-};
 
 server.use(cors(corsOptions));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-server.use(jsonServer.bodyParser);
-
-server.use(middlewares);
-server.use('', router);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+server.use(middlewares);
+server.use('', router);
 
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running');
